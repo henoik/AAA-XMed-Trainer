9b161fcde5bbf8e99de28aabd0e540e7946eba66
John Boronkas <JDBoronkas@gmail.com>
2014-11-20 20:33:22 -0500

    - Added Inventory data structure.

--- AAA XMed Trainer/AXT Engine/Inventory.cs
+++ AAA XMed Trainer/AXT Engine/Inventory.cs
@@ -18,9 +18,10 @@ namespace AXT_Engine
         /// <summary>
         /// Fills this inventory with the inventory setup defined in the OptionsForm.
         /// </summary>
-        public void FillFromOptions()
+        /// <returns>True if the load was successful, false otherwise.</returns>
+        public bool FillFromOptions()
         {
-
+            return true;
         }
 
         /// <summary>
@@ -28,10 +29,30 @@ namespace AXT_Engine
         /// </summary>
         /// <param name="item">The name of the item to check for.</param>
         /// <param name="amount">Optional. How many of 'item' to look for (inclusive).</param>
-        /// <returns>True if this inventory contains at least 'amount' of 'item', false otherwise.</returns>
+        /// <returns>True if this inventory contains at least 'amount' of 'item' (inclusive), false otherwise.</returns>
         public bool Check(IItem item, ushort amount = 1)
         {
-            return true;
+            if (item == null)
+            {
+                return false;
+            }
+
+            ushort amountHeldInInventory;
+            if(inventory.TryGetValue(item, out amountHeldInInventory))
+            {
+                if (amountHeldInInventory >= amount)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
 
         /// <summary>
@@ -39,9 +60,36 @@ namespace AXT_Engine
         /// </summary>
         /// <param name="item">The name of the item to add.</param>
         /// <param name="amount">How many of the item to add.</param>
-        public void Add(IItem item, ushort amount = 1)
+        /// <returns>True if the add was successful, false otherwise.</returns>
+        public bool Add(IItem item, ushort amount = 1)
         {
+            if (item == null || amount == 0)
+            {
+                return false;
+            }
 
+            if (inventory.ContainsKey(item))
+            {
+                ushort amountHeldInInventory;
+                if (inventory.TryGetValue(item, out amountHeldInInventory))
+                {
+                    inventory.Remove(item);
+
+                    amountHeldInInventory += amount;
+                    inventory.Add(item, amountHeldInInventory);
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                inventory.Add(item, amount);
+                return true;
+            }
         }
 
         /// <summary>
@@ -49,9 +97,38 @@ namespace AXT_Engine
         /// </summary>
         /// <param name="item">The name of the item to remove.</param>
         /// <param name="amount">How many of the item to remove.</param>
-        public void Remove(IItem item, ushort amount = 1)
+        /// <returns>True if the remove was successful, false otherwise.</returns>
+        public bool Remove(IItem item, ushort amount = 1)
         {
+            if (item == null || amount == 0)
+            {
+                return false;
+            }
 
+            if (inventory.ContainsKey(item))
+            {
+                ushort amountHeldInInventory;
+                if (inventory.TryGetValue(item, out amountHeldInInventory))
+                {
+                    inventory.Remove(item);
+
+                    int newAmount = amountHeldInInventory - amount;
+                    if (newAmount > 0)
+                    {
+                        inventory.Add(item, (ushort)newAmount);
+                    }
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
 
         /// <summary>
@@ -60,9 +137,37 @@ namespace AXT_Engine
         /// <param name="inventory">The inventory to transfer to.</param>
         /// <param name="item">The name of the item to transfer.</param>
         /// <param name="amount">How many of the item to transfer.</param>
-        public void Transfer(Inventory inventory, IItem item, ushort amount = 1)
+        /// <returns>True if the transfer was successful, false otherwise.</returns>
+        public bool Transfer(Inventory inventory, IItem item, ushort amount = 1)
         {
+            if (inventory == null || item == null || amount == 0)
+            {
+                return false;
+            }
 
+            if (Check(item, amount))
+            {
+                if (Remove(item, amount))
+                {
+                    if (inventory.Add(item, amount))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        Add(item, amount);
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
